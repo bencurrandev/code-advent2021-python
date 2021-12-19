243ff2e54352406689c584a439cb5b30d18a04fe
Ben Curran <ben@bencurran.dev>
2021-12-19 08:24:05 +0000

    WIP Day 3 A

---
+++ day03/day03.py
@@ -0,0 +1,8 @@
+"""
+This checks for power consumption
+Classes: None
+Functions: None
+"""
+with open('input.txt', encoding='UTF-8') as inputfile:
+    data = [int(datalen) for datalen in inputfile.readlines()]
+
