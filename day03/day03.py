0edbd4db96dc2785349ee95baff816bf0886b6d7
Ben Curran <ben@bencurran.dev>
2021-12-19 13:56:55 +0000

    Day 3 A Done

--- day03/day03.py
+++ day03/day03.py
@@ -4,5 +4,19 @@ Classes: None
 Functions: None
 """
 with open('input.txt', encoding='UTF-8') as inputfile:
-    data = [int(datalen) for datalen in inputfile.readlines()]
+    data = [str(datalen) for datalen in inputfile.readlines()]
 
+BITS = 12
+GAMMABIN = ""
+EPSILONBIN = ""
+for k in range(BITS):
+    BINCOUNT = 0
+    for binbit in data:
+        BINCOUNT += int(binbit[k])
+    result = round(BINCOUNT/len(data))
+    GAMMABIN += str(result)
+    EPSILONBIN += str(1-result)
+GAMMA = int(GAMMABIN, 2)
+EPSILON = int(EPSILONBIN, 2)
+POWER = GAMMA * EPSILON
+print(POWER)
