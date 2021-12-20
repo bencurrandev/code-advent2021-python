0ed5bf7dd622d4b66ebf860bb8ed9fccd935669b
Ben Curran <ben@bencurran.dev>
2021-12-20 02:33:24 +0000

    Day 3 B complete

--- day03/day03.py
+++ day03/day03.py
@@ -20,3 +20,25 @@ GAMMA = int(GAMMABIN, 2)
 EPSILON = int(EPSILONBIN, 2)
 POWER = GAMMA * EPSILON
 print(POWER)
+
+O2 = data
+CO2 = data
+for k in range(BITS):
+    if len(O2) > 1:
+        O2A = len([x for x in O2 if x[k]=='0'])
+        O2B = len([x for x in O2 if x[k]=='1'])
+        if O2B >= O2A:
+            O2 = [x for x in O2 if x[k]=='1']
+        else:
+            O2 = [x for x in O2 if x[k]=='0']
+    if len(CO2) > 1:
+        CO2A = len([x for x in CO2 if x[k]=='0'])
+        CO2B = len([x for x in CO2 if x[k]=='1'])
+        if CO2B >= CO2A:
+            CO2 = [x for x in CO2 if x[k]=='0']
+        else:
+            CO2 = [x for x in CO2 if x[k]=='1']
+OXYGEN = int(O2[0].strip(),2)
+CARBDIOX = int(CO2[0].strip(),2)
+LIFE = OXYGEN * CARBDIOX
+print(LIFE)
